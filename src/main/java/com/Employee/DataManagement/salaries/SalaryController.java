119de44570b3fd0f2c051a7206dab5f40f5ffe26
Arthur Dong <dyc1992@gmail.com>
2017-10-07 23:09:35 -0400

    Implement table "department" in the project
    Lmplemented and linked table "department" from the MySQL database to this spring boot application.

--- src/main/java/com/Employee/DataManagement/salaries/SalaryController.java
+++ src/main/java/com/Employee/DataManagement/salaries/SalaryController.java
@@ -18,20 +18,20 @@ import com.Employee.DataManagement.employees.Employee;
 
 
 @RestController
-@RequestMapping("employees")
+@RequestMapping("employees/{emp_no}/salaries")
 public class SalaryController {
 	
 	@Autowired
 	private SalaryService salaryService;
 	
-	@RequestMapping(value="/{emp_no}/salaries")
+	@RequestMapping(value="")
 	public @ResponseBody List<Salary> getSalariesByEmp_no(@PathVariable @Valid String emp_no) {
 		Integer empNo = Integer.parseInt(emp_no);
 //		System.out.println("emp no: "+empNo);
 		return salaryService.getSalariesByEmp_no(empNo);
 	}
 	
-	@RequestMapping(value="/{emp_no}/salaries/from_date_{from_date}")
+	@RequestMapping(value="/from_date_{from_date}")
 	// string date format should be like 1992-02-13 (yyyy-MM-dd)
 	public @ResponseBody Salary getSalariesByFromDate(@PathVariable("from_date") String fromDate) throws ParseException {
 		java.sql.Date sqlDate = salaryService.str2SDate(fromDate);
@@ -39,33 +39,33 @@ public class SalaryController {
 	}
 	
 	
-	@RequestMapping(value="/{emp_no}/salaries/to_date_{to_date}")
+	@RequestMapping(value="/to_date_{to_date}")
 	public @ResponseBody List<Salary> getSalariesByToDate(@PathVariable("to_date") String toDate) throws ParseException {
 		java.sql.Date sqlDate = salaryService.str2SDate(toDate);
 		return salaryService.getSalariesByToDate(sqlDate);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/{emp_no}/salaries")
+	@RequestMapping(method=RequestMethod.POST, value="")
 	public void addSalary(@RequestBody Salary salary, @PathVariable("emp_no") Integer empNo) {
 		salary.setEmbeddedKeyId(new EmbeddedKeyId(salary.getEmbeddedKeyId().getFromeDate(), new Employee(empNo, null, null, null, null, null)));
 		salaryService.addSalary(salary);;
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/{emp_no}/salaries/{from_date}")
+	@RequestMapping(method=RequestMethod.PUT, value="/from_date_{from_date}")
 	public void updateSalary(@RequestBody Salary salary, @PathVariable("emp_no") String empNo, @PathVariable("frome_date") String fromDate) throws ParseException {
 		Integer _empNo = Integer.parseInt(empNo);
 		salary.setEmbeddedKeyId(new EmbeddedKeyId(salary.getEmbeddedKeyId().getFromeDate(), new Employee(_empNo, null, null, null, null, null)));
 		salaryService.updateSalary(salary);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/{emp_no}/salaries/{from_date}")
+	@RequestMapping(method=RequestMethod.DELETE, value="/from_date_{from_date}")
 	public void deleteSalayByFromDate(@PathVariable("from_date") String fromeDate, @PathVariable("emp_no") Integer empNo) throws ParseException {
 		java.sql.Date sqlDate = salaryService.str2SDate(fromeDate);
 		salaryService.deleteSalariesByFromDate(sqlDate);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/{emp_no}/salaries")
-	public void deleteAllSalariesUnderEmployee(@PathVariable String empNo) {
+	@RequestMapping(method=RequestMethod.DELETE, value="")
+	public void deleteAllSalariesUnderEmployee(@PathVariable("emp_no") String empNo) {
 		Integer intEmpNo = Integer.parseInt(empNo);
 		salaryService.deleteAllSalariesUnderEmployee(intEmpNo);
 	}
